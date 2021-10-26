c3dd133eef3a047d31dcaf33683d6807af73a22a
SeinopSys <seinopsys@protonmail.com>
2021-10-26 19:53:21 +0200

    Upgrade to Next.js 12

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const { i18n } = require('./next-i18next.config.js');
 const withPlugins = require('next-compose-plugins');
-const withESLint = require('./utils/next-eslint');
 const withCamelCaseCSSModules = require('./utils/next-css-modules');
 
-module.exports = withPlugins([[withESLint], [withCamelCaseCSSModules]], {
+module.exports = withPlugins([[withCamelCaseCSSModules]], {
   i18n,
 });
